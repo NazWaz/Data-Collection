2cf61d1643a325c7608f81b3cc4e3d62d7c8fd00
NazWaz <wazidur123@gmail.com>
2022-12-09 23:12:38 +0000

    adds requirements.txt

--- scraper.py
+++ scraper.py
@@ -3,8 +3,7 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
 from webdriver_manager.chrome import ChromeDriverManager
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
+
 from datetime import datetime
 from unidecode import unidecode
 
